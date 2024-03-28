b27a0eb91f70f1ed261735b8d419aab06650625c
Karolina Nowak <nowakina15@gmail.com>
2024-03-28 18:35:27 +0100

    final

--- stare nie dziala/Lab2v2/Lab2v2/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Lab2v2 {
-    internal class Program {
-        static void Main(string[] args) {
-            GetAPI api = new GetAPI();
-            api.GetData().Wait();
-        }
-    }
-}
